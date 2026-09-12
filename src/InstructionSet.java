b04cec0f3059a09969d5867bad65c74f0c08789d
Muhammed Hisham M H <87166264+Hisham-Muhammed@users.noreply.github.com>
2026-09-12 18:47:25 +0530

    Add stack and FIFO queue instructions to categories

--- src/InstructionSet.java
+++ src/InstructionSet.java
@@ -26,11 +26,15 @@ public class InstructionSet {
         // Control Flow
         instructionCategories.put("SJMP", "Control Flow");
 
-        // Team marker / project identity command
-        instructionCategories.put("ONJI", "Team Marker");
 
         // Program Termination
         instructionCategories.put("END", "Program Termination");
+
+        // Week 3: Stack and FIFO Queue
+        instructionCategories.put("PUSH", "Stack");
+        instructionCategories.put("POP", "Stack");
+        instructionCategories.put("ENQUEUE", "FIFO Queue");
+        instructionCategories.put("DEQUEUE", "FIFO Queue");
     }
 
     public boolean isSupported(String instruction) {
